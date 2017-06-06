7c0b5e5da9fda2a63c4923c107d7ab2657863dcf
wangbing <wangbingit@outlook.com>
2017-06-06 23:33:09 +0800

    init

--- app/src/main/java/com/cyber/lbsservice/MainActivity.java
+++ app/src/main/java/com/cyber/lbsservice/MainActivity.java
@@ -1,10 +1,8 @@
 package com.cyber.lbsservice;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
 
 import com.baidu.location.BDLocation;
 import com.baidu.location.BDLocationListener;
@@ -14,7 +12,7 @@ import com.baidu.location.Poi;
 
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
     private LocationClient mLocationClient;
     public BDLocationListener myListener = new BDLocationListener(){
 
@@ -168,26 +166,4 @@ public class MainActivity extends AppCompatActivity {
         mLocationClient.setLocOption(option);
     }
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
